e57ee624bbc685029ee7b35c5a70e7c29ff53727
the_deeppp <deepbtrivedi04@gmail.com>
2025-03-26 14:08:07 +0530

    init routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter, Routes, Route }from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <h1>hola</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/"/>
+          <Route path = "/"/>
+        </Routes>
+      </BrowserRouter>
+    </div>  
     </>
   )
 }
